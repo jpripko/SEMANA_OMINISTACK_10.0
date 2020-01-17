a7c92b87480809ef7c8793fb69095e5bccfcc11c
jpripko <jeanpripko@hotmail.com>
2020-01-17 10:54:53 -0300

    add Socket na Main

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,6 +4,7 @@ import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import {MaterialIcons} from '@expo/vector-icons'
 import api from '../services/api'
+import socket from '../services/socket'
 
 function Main({navigation}){
     const[devs, setDevs] = useState([]);
